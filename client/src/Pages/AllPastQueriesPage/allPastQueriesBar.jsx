27bfe95361012c573005025677a312985efbeb97
SMantri05 <sumit@instaservice.com>
2025-09-17 12:51:06 -0700

    Displaying the queries

---
+++ client/src/Pages/AllPastQueriesPage/allPastQueriesBar.jsx
@@ -0,0 +1,71 @@
+import { set } from "mongoose";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useUser } from '../../Components/context/UserContext.js'
+import displayOnePastQuery from '../../Components/displayPastQuery/displayOnePastQuery.jsx'
+function allPastQueriesPage() {
+    const userInfo = useUser();
+    const{ googleID } = userInfo.user;
+    const [queryMap, setQueryMap] = useState(new Map());
+    const [index, setIndex] = useState(0); // This will be used to keep track of the index of the queries
+    
+    const [currentQuery, setCurrentQuery] = useState(null); // This will be used to display the current query
+    useEffect(() => {
+        async () => {
+            await gettingDocuments();
+            displayingAllQueries();
+        }
+    }, []); 
+
+    const gettingDocuments = async () => {
+        let allQueries;
+        try {
+            allQueries = await axios.get(`/users/getAllDocments/${googleID}`);
+        } catch (error) {
+            console.error("Error fetching past queries:", error);
+            return;
+        }
+        for(let i = 0; i < allQueries.data.pastQueries.length; i++) {
+            const tempQuery = allQueries.data.pastQueries[i];
+            setQueryMap(prevMap => {
+                const newMap = new Map(prevMap);
+                newMap.set(i, tempQuery);
+                return newMap;
+            });
+            // Do something with each query object
+        }
+        setCurrentQuery(prevValue => queryMap.get(index));    
+    }
+    const displayingAllQueries = () => {
+        const queriesList = [];
+        for (const [key, value] of queryMap) {
+            const index = value.originalResumeKey.indexOf('-');
+            const queryName = value.originalResumeKey.substring(0, index);
+            queriesList.push(<li key={key}>{queryName}</li>);
+        }
+        return <ul>{queriesList}</ul>;
+    };
+    return (
+        <>
+        
+            <div>
+                <h2> Past Queries Bar </h2>
+                {displayingAllQueries()}
+            </div>
+            <div>
+                <button> Previous </button>
+                <button> Next </button>
+            </div>
+            <div>
+                <displayOnePastQuery 
+                    googleID={googleID} 
+                    originalResumeURL={queryMap.get(index)?.originalResumeURL}
+                    parsedResumeURL={queryMap.get(index)?.parsedResumeURL}
+                    jobDescription={queryMap.get(index)?.jobDescription}
+                />
+            </div>
+        </>
+    );
+}
+export default allPastQueriesPage;
--- client/src/Pages/ApplicationPage/filesSending.jsx
+++ client/src/Pages/ApplicationPage/filesSending.jsx
@@ -2,21 +2,25 @@ import React, {useState} from "react";
 import JobDescription from '../../Components/JobDescription/JobDescription.jsx'
 import displayPastQuery from '../../Components/displayPastQuery/displayOnePastQuery.jsx'
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 // import { useState } from "react";
 
 //show previous queries
+
 function filesSending() {
     //const [u]
-    
+    const navigate = useNavigate();
+    const goToPastQueries = () => {
+        navigate('/pastQueries');
+    }
     return(
         <>
             <h1> Files Sending </h1>
-            <div>
-                Previous Queries
-                
-            </div>
             <div> 
+                <div>
+                    <button onClick={goToPastQueries}> Past Queries </button>
+                </div>
                 <JobDescription/>
             </div>
         </>
