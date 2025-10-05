5fb0c3872ba52acc4bd6f77c78a2df83bd2b3dc2
SMantri05 <sumit@instaservice.com>
2025-10-04 20:44:58 -0700

    Made changes to render-build.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
-curl -L https://github.com/tectonic-typesetting/tectonic/releases/download/latest/tectonic-x86_64-unknown-linux-gnu.tar.gz | tar xz -C .
+
+# Download the correct tectonic release (update version if needed)
+curl -L -o tectonic.tar.gz https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%400.14.1/tectonic-0.14.1-x86_64-unknown-linux-musl.tar.gz
+
+# Extract the tarball
+tar -xzf tectonic.tar.gz
+
+# Move the tectonic binary to the project root
+mv tectonic-0.14.1-x86_64-unknown-linux-musl/tectonic .
+
+# Make it executable
 chmod +x ./tectonic
+
+# Clean up
+rm -rf tectonic-0.14.1-x86_64-unknown-linux-musl tectonic.tar.gz
